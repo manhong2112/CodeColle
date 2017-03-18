def994946059acc233fe959a846212fd771a55d6
manhong2112 <manhong2112@gmail.com>
2017-03-19 01:37:44 +0800

    Script commit at 19/03/2017  1:37:43 UTC+8

--- Python/st/interp.py
+++ Python/st/interp.py
@@ -1,3 +1,6 @@
+"""
+參照某天跟冰封提起的方法嘗試實現的一個解釋器
+"""
 # parser str to list
 def parser(s):
     def _f(index):
@@ -24,4 +27,47 @@ def parser(s):
             index += 1
     return _f(1)[0]
 
-print(parser("(+(+ 1 1) 1)"))
+class Closure():
+    def __init__(self, func, scope):
+        # TODO
+        self.argsList = []
+        self.argsLen = len(argsList)
+        self.func = func
+        self.scope = scope
+        self.runtime = 0
+    
+    def invoke(self, args, env):
+        for i in range(0, self.argsLen):
+            
+
+class PreDefFunc():
+    def invoke(args, env):
+        for i in range(0, self.argsLen):
+
+class Env():
+    def __init__(self):
+        self.env = dict()
+    def get(self, scope, name):
+        while True:
+            try:
+                return self.env[(name, scope)]
+            except KeyError:
+                scope = scope[1]
+                if scope is None:
+                    return None
+    def set(self, scope, name, val):
+        self.env[(name, scope)] = val
+
+def interp0(expr, env, scope):
+    if expr is list
+        if expr[0] is list:
+            e0 = interp(expr, env, scope)
+        fun = expr[interp(expr, env, scope)]
+        for i in expr:
+    else:
+        return (env.get(scope, expr), None)
+
+def interp(expr):
+    interp0(expr, Env(), None)[0]
+# [+ [+ 1 1] 1]
+print(interp(parser("(+(+ 1 1) 1)"), Env(), None))
