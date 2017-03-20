97aafae2702b2acd52557eb8466cf4daf88b80e2
manhong2112 <manhong2112@gmail.com>
2017-03-20 20:48:50 +0800

    Script commit at 20/03/2017 20:48:50 UTC+8

--- push.cmd
+++ push.cmd
@@ -1,9 +1,5 @@
 @echo off
 cd C:\manhong\WorkSpace\Code
 git add .
-IF [%1] EQU [] (
-git commit -m "Script auto commit at %DATE:~0,10% %TIME:~0,8% UTC+8"
-) ELSE (
-git commit -m "%1% %DATE:~0,10% %TIME:~0,8% UTC+8"
-)
+git commit -m "Script commit at %DATE:~0,10% %TIME:~0,8% UTC+8"
 git push github --force
