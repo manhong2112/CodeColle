d364d767d65aee35304d6d44740e0aac5a8474b3
manhong2112 <manhong2112@gmail.com>
2017-03-27 00:00:02 +0800

    Script commit at 27/03/2017  0:00:02 UTC+8

--- Rust/main.rs
+++ Rust/main.rs
@@ -7,14 +7,19 @@ use std::collections::LinkedList;
 use std::io::stdin;
 
 struct Step {
-    x: i8,
-    y: i8,
+    loc: Loc,
     who: i8
 }
+
+struct Loc {
+    x: i8,
+    y: i8,
+}
+
 struct EachRecord {
-    win: Vec<i8>,
-    draw: Vec<i8>,
-    lose: Vec<i8>
+    win: Vec<Loc>,
+    draw: Vec<Loc>,
+    lose: Vec<Loc>
 }
 fn main() {
     let mut table : LinkedList<Step> = LinkedList::new();
@@ -23,6 +28,7 @@ fn main() {
         if end_check(&table) {
             table.clear();
         }
+        print(&table);
         let mut input = String::new();
         match stdin().read_line(&mut input) {
             Ok(_) => {},
@@ -32,27 +38,86 @@ fn main() {
         let x = tmp[0].parse::<i8>().unwrap();
         let y = tmp[1].parse::<i8>().unwrap();
         check(&mut table, x, y, 1);
-        print(&table);
     }
 }
 
 fn check(table: &mut LinkedList<Step>, x: i8, y: i8, who: i8) {
-    table.push_back(Step { x: x, y: y, who: who});
+    table.push_back(Step { loc: Loc{x:x, y:y}, who: who});
 }
 
 fn end_check(table: &LinkedList<Step>) -> bool {
-    return table.len() > 9;
+    return table.len() > 8;
 }
 
-fn add2record(table: &LinkedList<Step>, record: &mut [EachRecord; 19683]) {
+fn add2record(table: &[[i8; 3]; 3], record: &mut [EachRecord; 19683]) {
+    // Step(x, y, who) -> EachRecord[table]: 
+    //                      win: Vec<Loc NextStep>,  draw: Vec<Loc>,  lose: Vec<Loc>
+    let t = table_status(table);
+}
 
+fn table_status(table: &[[i8; 3]; 3]) -> i8 {
+    // ret
+    // 0 = unfinished
+    // 1 = win 0
+    // 2 = win 1
+    // 3 = draw
+
+    // xxx * 3 x      x
+    //         x * 3   x  * 3
+    //         x        x
+    let t1 = test1(&table);
+    let t2 = test2(&table);
+    let t3 = test3(&table);
+    return 0;
+}
+
+fn test1(table: &[[i8; 3]; 3]) -> i8 {
+    for i in 0 .. 3 {
+        if table[i][0] != 0 &&
+           table[i][0] == table[i][1] &&
+           table[i][1] == table[i][2] {
+            return table[i][0];
+        }
+    }
+    return 0;
+}
+
+fn test2(table: &[[i8; 3]; 3]) -> i8 {
+    for i in 0 .. 3 {
+        if table[0][i] != 0 &&
+           table[1][i] == table[0][i] &&
+           table[2][i] == table[1][i] {
+            return table[0][i];
+        }
+    }
+    return 0;
+}
+
+fn test3(table: &[[i8; 3]; 3]) -> i8 {
+    //  y
+    //  2 *|_|*
+    //  1 _|*|_
+    //  0 *| |*
+    //    0 1 2 x
+    if table[0][0] != 0 && table[0][0] == table[1][1] && table[1][1] == table[2][2] {
+        return table[0][0];
+    } else if table[2][0] != 0 && table[2][0] == table[1][1] && table[1][1] == table[0][2] {
+        return table[2][0];
+    } else {
+        return 0;
+    }
+}
+
+fn steplist2array(table: &LinkedList<Step>) -> [[i8; 3]; 3] {
+    let mut t : [[i8; 3]; 3] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    for i in table.iter() {
+        t[i.loc.x as usize][i.loc.y as usize] = i.who;
+    }
+    return t;
 }
 
 fn print(table: &LinkedList<Step>) {
-    let mut t : [[i8; 3]; 3] = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-    for i in table.iter() {
-        t[i.x as usize][i.y as usize] = i.who;
-    }
+    let t = steplist2array(&table);
     for y in (0..3).rev() {
         for x in 0..3 {
             print!("{}", t[x][y]);
