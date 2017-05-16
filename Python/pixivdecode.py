7cfb56fda62e9f66cf694708b04e1378f34e8b09
manhong2112 <manhong2112@gmail.com>
2017-05-16 21:01:51 +0800

    Script commit at 16/05/2017 21:01:51 UTC+8

--- Python/pixivdecode.py
+++ Python/pixivdecode.py
@@ -1,13 +1,26 @@
 import urllib.request as request
 from urllib.error import HTTPError
 import re
+import sys
 
 GET = lambda url: request.urlopen(url)
 # http://www.pixiv.net/img-original/img/<time>/<pid>_p<page>.png
 
+def argsParse(args):
+    # xxx=yyy | xxx
+    res = {}
+    for i in args:
+        i = i.split("=", 1)
+        if len(i) == 1:
+            res[i[0]] = True
+        else:
+            res[i[0]] = i[1]
+    return res
+
+
 
 def getImg(date, pid, page, ext):
-    url = "http://i1.pixiv.net/img-original/img/{}/{}_p{}.{}"\
+    url = "https://i.pximg.net/img-original/img/{}/{}_p{}.{}"\
         .format(date, pid, page, ext)
     req = request.Request(url, headers={'referer': url})
     return request.urlopen(req).read()
@@ -36,12 +49,18 @@ def main(pid):
                 ext = "jpg"
                 continue
 
-while True:
-    pid = input("pid > ")
+# py xxx dist=123 "pid=123456 123456"
+args = argsParse(sys.argv)
+assert "pid" in args
+if "dist" in args:
+    dist = args["dist"]
+else:
+    dist = None
+for pid in args["pid"].split(" "):
     page = 0
     for p in main(pid):
-        dist = "{}_p{}.png".format(pid, page)
+        dist = f"{dist + '/' if dist else ''}{pid}_p{page}.png"
         with open(dist, 'wb') as f:
             f.write(p)
-            print("saved to {}".format(dist))
+            print(f"saved to {dist}")
         page += 1
