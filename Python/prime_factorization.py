3d9b215bf4401873841b5fbb0814e7e65559f5f7
manhong2112 <manhong2112@gmail.com>
2017-07-27 00:00:06 +0800

    Script commit at 27/07/2017  0:00:04 UTC+8

--- Python/prime_factorization.py
+++ Python/prime_factorization.py
@@ -1,7 +1,7 @@
 from functools import *
 def f(x):
     k = int(x ** 0.5)
-    def _f(x, n=3):
+    def _f(x, n):
         if n > k:
             return [x]
         elif x % n == 0:
@@ -9,6 +9,6 @@ def f(x):
         else:
             return _f(x, n+2)
     if x % 2 == 0:
-        return [2] + f(x//2)
+        return [2] + f(x//2, 3)
     else:
-        return _f(x)
+        return _f(x, 3)
