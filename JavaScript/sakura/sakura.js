b5071d9e0f196bbc8a7e5f342f7d15dda5ca390a
manhong2112 <manhong2112@gmail.com>
2017-08-08 23:36:10 +0800

    Script commit at 08/08/2017 23:36:08 UTC+8

--- JavaScript/sakura/sakura.js
+++ JavaScript/sakura/sakura.js
@@ -1,12 +1,29 @@
 "use strict"
 // k / (150 * size)
-var createCanvas = (w, h) => {
-   var c = document.createElement('canvas');
-   c.width = w
-   c.height = h
-   return c
+var createElement = (tag, prop, style) => {
+   let ele = document.createElement(tag)
+   for (let i in (prop || [])) {
+      ele[i] = prop[i]
+   }
+   for (let i in (style || [])) {
+      ele.style[i] = style[i]
+   }
+   return ele
 }
-var getSakura = (size) => {
+var createCanvas = (w, h) => {
+   return createElement('canvas', {"width": w, "height": h})
+}
+
+{
+   let style = createElement("style")
+   document.body.appendChild(style)
+   let stylesheet = style.sheet
+   var addStyle = (rule) => {
+      stylesheet.insertRule(rule, stylesheet.cssRules.length);
+   }
+}
+
+var drawSakura = (size) => {
    let canvas = createCanvas(size, size)
    let scale = 150 / size
    if (canvas.getContext) {
@@ -34,3 +51,72 @@ var getSakura = (size) => {
    return canvas
 }
 
+var createSakura = () => {
+   let o = {
+      size: randint(5, 80),
+      vx: rand(0.2, 1.5),
+      vy: rand(0.3, 2),
+      rotatev: (Boolean(randint(0, 1)) ? -1 : +1) * rand(0.1, 0.5),
+   }
+   o.c = drawSakura(o.size)
+   o.move = () => {
+      o.x += o.vx
+      o.y += o.vy
+      if (o.x > window.innerWidth) {
+         o.x = -50
+      }
+      if (o.y > window.innerHeight) {
+         o.y = -50
+      }
+   }
+   o.rotate = () => {
+      o.rotatex += o.rotatevx
+      o.rotatey += o.rotatevy
+      o.rotatez += o.rotatevz
+      if (o.rotatex > 360) {
+         o.rotatex -= 360
+      } else if (o.rotatex < 0) {
+         o.rotatex += 360
+      }
+      if (o.rotatey > 360) {
+         o.rotatey -= 360
+      } else if (o.rotatey < 0) {
+         o.rotatey += 360
+      }
+      if (o.rotatez > 360) {
+         o.rotatez -= 360
+      } else if (o.rotatez < 0) {
+         o.rotatez += 360
+      }
+   }
+   return o
+}
+
+var drawAnimation = (list, speed) => {
+   // init
+   let div = createElement('div', [], {"width": "100%", "height": "100%", "position": "fixed", "overflow": "hidden"})
+   document.body.appendChild(div)
+   list.forEach((obj, _index, _arr) => {
+      obj.c.style.position = "absolute"
+      let scalePosX = 1920 / speed
+      let scalePosY = 1080 / speed
+      let scaleRotate = 360 / speed
+      obj.c.style.opacity = `${obj.size / 280}`
+      obj.c.style.filter = `blur(${obj.size / 240}px)`
+      obj.c.style.animation =
+         `pX ${scalePosX / obj.vx}s infinite linear, \
+          pY ${scalePosX / obj.vy}s infinite linear, \
+          r ${scaleRotate / Math.abs(obj.rotatev)}s infinite linear ${obj.rotatev > 0 ? "" : "backwards"}`
+      div.appendChild(obj.c)
+   })
+}
+
+var sakura_main = (num, speed) => {
+   addStyle("@keyframes pX {0% {left: -100px;} 100% {left: 100%;}}")
+   addStyle("@keyframes pY {0% {top: -100px;} 100% {top: 100%;}}")
+   addStyle("@keyframes r {0% {rotateX(0deg) rotateY(0deg) rotateZ(0deg);} \
+                         100% {rotateX(360deg) rotateY(360deg) rotateZ(360deg);}}")
+   var sakuraList = window.sakuraList = initArray(num || 300, createSakura)
+   drawAnimation(sakuraList, speed || 60)
+}
+window.onload = () => { sakura_main(300) }
