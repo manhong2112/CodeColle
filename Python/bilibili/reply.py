e03a6e3a668ebf82e411dea3a1bc27fe7d62429f
manhong2112 <manhong2112@gmail.com>
2017-08-15 00:00:07 +0800

    Script commit at 15/08/2017  0:00:05 UTC+8

--- Python/bilibili/reply.py
+++ Python/bilibili/reply.py
@@ -4,7 +4,7 @@ from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 import json
 import time
-
+from pushbullet import PushBullet
 
 def POST(url, header=dict(), data=dict()):
     request = Request(url, urlencode(data).encode())
@@ -32,6 +32,13 @@ def json2ReplyData(json, subreply=None):
                      message=json['content']['message'],\
                      reply=subreply)
 
+def json2subReplyData(json, subreply=None):
+    subreply = {}
+    for each_subreply in json:
+        subreply_data = json2ReplyData(each_subreply)
+        subreply[subreply_data.floor] = subreply_data
+    return subreply
+
 class Reply(object):
     def __init__(self, aid):
         self.aid = aid
@@ -52,19 +59,21 @@ class Reply(object):
         url = "https://api.bilibili.com/x/v2/reply?type=1&oid={}&pn={}" # aid, page number
         
         res = [get_json(url.format(self.aid, 1))]
+        reply = {}
+        
+        upper_subreply_data = json2subReplyData(res[0]["data"]["upper"]["top"]["replies"])
+        upper_reply_data = json2ReplyData(res[0]["data"]["upper"]["top"], upper_subreply_data)
+        reply[upper_reply_data.floor] = upper_reply_data
+
         reply_count = self.last_reply_count = res[0]["data"]["page"]["count"]
         self.last_reply_acount = res[0]["data"]["page"]["acount"]
         for pn in range(2, reply_count // 20 + 2):
             res.append(get_json(url.format(self.aid, pn)))
 
-        reply = {}
         for each_page in res:
             for each_reply in each_page["data"]["replies"]:
-                subreply = {}
-                for each_subreply in each_reply["replies"]:
-                    subreply_data = json2ReplyData(each_subreply)
-                    subreply[subreply_data.floor] = subreply_data
-                reply_data = json2ReplyData(each_reply, subreply)
+                subreply_data = json2subReplyData(each_reply["replies"])
+                reply_data = json2ReplyData(each_reply, subreply_data)
                 reply[reply_data.floor] = reply_data
         self.last_reply = reply
         return reply
@@ -74,11 +83,8 @@ class Reply(object):
         res = get_json(url.format(self.aid, pn))
         reply = {}
         for each_reply in res["data"]["replies"]:
-            subreply = {}
-            for each_subreply in each_reply["replies"]:
-                subreply_data = json2ReplyData(each_subreply)
-                subreply[subreply_data.floor] = subreply_data
-            reply_data = json2ReplyData(each_reply, subreply)
+            subreply_data = json2ReplyData(each_reply["replies"])
+            reply_data = json2ReplyData(each_reply, subreply_data)
             reply[reply_data.floor] = reply_data
         return reply
 
@@ -88,15 +94,25 @@ class Reply(object):
     def getLastReply(self):
         return self.last_reply
 
-def main(aid):
+
+
+def push_notication(PB, title, message):
+    for i in PB.getDevices():
+        if i["pushable"]:
+            PB.pushNote(i["iden"], title, message)
+
+def main(aid, pb):
     k = Reply(aid)
     last_reply_count = 0
+    count = 0
     while True:
         l = k.getReplyaCount()
-        while l <= last_reply_count:
-            time.sleep(5)
+        while l == last_reply_count:
+            time.sleep(60 * 5)
             l = k.getReplyaCount()
+        if l > last_reply_count:
+            raw = k.getReply()
+            s = "\n".join(repr(raw[i]) for i in raw)
+            open(f"reply.{count}", "w", encoding="utf-8").write(s)
+            count += 1
         last_reply_count = l
-        g = k.getReply()
-        for i in g:
-            print(g[i])
