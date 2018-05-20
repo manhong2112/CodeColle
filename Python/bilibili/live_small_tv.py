e33d7712cf59f411484f8e24297ee09012bf4f03
manhong2112 <manhong2112@gmail.com>
2018-05-20 13:29:40 +0800

    Script commit at 20/05/2018 13:29:37 UTC+8

--- Python/bilibili/live_small_tv.py
+++ Python/bilibili/live_small_tv.py
@@ -1,17 +1,9 @@
-"""Accept: application/json, text/plain, */*
-Accept-Encoding: gzip, deflate, br
-Accept-Language: zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7
-Connection: keep-alive
-Content-Length: 93
-Content-Type: application/x-www-form-urlencoded
-User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.183 Safari/537.36 Vivaldi/1.96.1147.36
-
-{"cmd":"SYS_MSG","msg":"","msg_text":"","rep":1,"styleType":2,"url":"http://live.bilibili.com/8498920","roomid":8498920,"real_roomid":8498920,"rnd":176055739,"tv_id":"49044"}
-"""
 import live_chat
 import live_tool
 import utils as ut
 import json
+import numpy as np
+import time
 
 
 def join(msg, cookie):
@@ -22,25 +14,32 @@ def join(msg, cookie):
    return ut.GET(
        url,
        headers={
-           "Cookie": cookie,
-           "Origin": "https://live.bilibili.com",
-           "Referer": f"https://live.bilibili.com/{shortid}",
+           "Cookie":
+           cookie,
+           "Origin":
+           "https://live.bilibili.com",
+           "Referer":
+           f"https://live.bilibili.com/{shortid}",
+           "User-Agent":
+           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.183 Safari/537.36 Vivaldi/1.96.1147.36",
        })
 
 
 def main():
+   cookie = ""
    room = live_tool.Live(184298).get_chat_room()
    while True:
       msg = room.next()["content"]
       try:
          msg = json.loads(msg)
-         if msg["cmd"] == "SYS_MSG" and msg["rep"] == 1 and msg["styleType"] == 2:
+         if msg["cmd"] == "SYS_MSG" and msg["rep"] == 1 and msg["styleType"] == 2 and (
+             np.random.rand(1)[0] > 0.2):
             print("small tv?")
-            print(
-                join(
-                    msg,
-                    ""
-                ).read())
+            print(msg)
+            t = np.random.rand() * 5 + abs(np.random.normal(loc=20, scale=5))
+            print(f"sleep for {t}")
+            time.sleep(t)
+            print(join(msg, cookie).read())
       except Exception as e:
          print(e)
 
